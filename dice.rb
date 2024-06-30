5c839901aa58b9461ec96bf23b0e0e4e41744042
Stephen Steiner <46692873+stephensteiner4@users.noreply.github.com>
2024-06-30 21:03:46 +0000

    shift to bullet points on outcome of dice

--- dice.rb
+++ dice.rb
@@ -23,12 +23,12 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
+  @first_die = rand(1..6)
+  @second_die = rand(1..6)
 
-  sum = first_die + second_die
+  @sum = @first_die + @second_die
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{@first_die} and a #{@second_die} for a total of #{@sum}."
   
   erb(:two_six)
 end
@@ -52,6 +52,19 @@ get("/dice/3/20") do
 
   @outcome = "You rolled a #{dice_rolls[0]}, a #{dice_rolls[1]}, and a #{dice_rolls[2]} for a total of #{sum}."
   erb(:three_twenty)
+
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(100)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 
 end
 
